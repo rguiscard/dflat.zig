17bf07c0872fc50c60aacfc25396d7ebc06184b8
rguiscard <rguiscard@outlook.com>
2025-09-18 10:08:53 +0800

    add truncate and len to gapbuffer

--- src/GapBuffer.zig
+++ src/GapBuffer.zig
@@ -53,17 +53,18 @@ fn ensureGap(self: *TopLevelFields, need: usize) !void {
     if (gap_size >= need) return;
 
     const old_len = self.items.len - 1; // exclude sentinel
-    var new_len = old_len * 2;
-    if (new_len < old_len + need) new_len = old_len + need;
+    const new_len = @max(old_len * 2, old_len-gap_size+need);
+
 
     const right_len = old_len - self.gap_end;
     const new_gap_end = new_len - right_len;
     if (self.realloc) {
         if (self.allocator.realloc(self.items, new_len + 1)) |new_buf| {
-//            @memcpy(
-//                new_buf[new_gap_end..new_gap_end+right_len],
-//                self.items[new_gap_end..new_gap_end+right_len],
-//            );
+            // need to move right part to enlarge gap
+            @memcpy(
+                new_buf[new_gap_end..new_gap_end+right_len],
+                self.items[self.gap_end..self.gap_end+right_len],
+            );
             self.items = new_buf;
         } else |err| {
             return err;
@@ -102,6 +103,11 @@ pub fn insertSlice(self: *TopLevelFields, slice: []const u8) !void {
 }
 
 pub fn moveCursor(self: *TopLevelFields, pos: usize) void {
+    const l = self.len();
+    if (pos > l) {
+        return;
+    }
+
     if (pos < self.gap_start) {
         const left_move = self.gap_start - pos;
         @memmove(
@@ -144,13 +150,50 @@ pub fn compact(self: *TopLevelFields) void {
 }
 
 pub fn trancate(self: *TopLevelFields, pos: usize) void {
-    self.gap_start = pos;
-    self.gap_end = self.items.len - 1;
+    const l = self.len();
+    if (pos > l) {
+        return;
+    }
+    self.moveCursor(pos);
+    self.gap_end = self.items.len - 1; // everything after pos is discarded
+
+
+}
+
+pub fn len(self: *TopLevelFields) usize {
+    return (self.items.len - 1) - (self.gap_end - self.gap_start);
 }
 
 pub fn toString(self: *TopLevelFields) [:0]const u8 {
     self.compact();
     return self.items[0..self.gap_start:0];
+}
+
+test "gap buffer out of bound 1" {
+    const gpa = std.testing.allocator;
+    for (0..2) |idx| {
+        var buf = try TopLevelFields.init(gpa, 40);
+        defer buf.deinit();
+        if (idx > 0) {
+           buf.setRealloc(true);
+        }
+        try buf.insertSlice("Hello");
+        buf.moveCursor(20);
+        try std.testing.expectEqualStrings("Hello", buf.toString());
+        buf.trancate(20);
+        try std.testing.expectEqualStrings("Hello", buf.toString());
+
+        buf.clear();
+        try buf.insertSlice("0123456789");
+        buf.moveCursor(8);
+        for(0..6) |_| {
+            buf.backspace();
+        }
+        try std.testing.expectEqualStrings("0189", buf.toString());
+
+        buf.trancate(3); // middle of gap
+        try std.testing.expectEqualStrings("018", buf.toString());
+    }
 }
 
 test "gap buffer trancate" {
